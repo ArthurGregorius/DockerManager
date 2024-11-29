de27258559dd77b362855c6b10793aba83b870c5
Arthur Gregorius Pongoh <147962819+ArthurGregorius@users.noreply.github.com>
2024-11-30 04:55:33 +0700

    Update test_manager_docker.py

--- src/tests/test_manager_docker.py
+++ src/tests/test_manager_docker.py
@@ -10,21 +10,3 @@ def docker_client():
     yield client
     # Teardown: Optional, jika Anda ingin membersihkan container atau resources
     client.close()
-
-def test_get_docker_version(docker_client):
-    # Uji apakah get_docker_version menghasilkan output yang valid
-    version = get_docker_version()
-    assert "ApiVersion" in version
-    assert "Version" in version
-
-def test_create_container(docker_client):
-    # Uji apakah create_container membuat container yang benar
-    image_name = "alpine"
-    container_id = create_container(image_name)
-    assert container_id is not None
-    assert len(container_id) > 0
-
-    # Cleanup: Hapus container yang telah dibuat
-    container = docker_client.containers.get(container_id)
-    container.stop()
-    container.remove()
